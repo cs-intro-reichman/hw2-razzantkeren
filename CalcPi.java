bdde4d4ca8517a40d28ee51f4bf8e0db34f62027
razzantkeren <raz.zantkeren@post.runi.ac.il>
2024-11-18 23:15:20 +0200

    kkk

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,21 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		double dividedown = 3;
+		double app = 1;
+		for(int i = 0; i<num; i++){
+			if (i%2==0) {
+				app = app-(1/dividedown);
+			}
+			else{
+			app = app+(1/dividedown);
+			}
+			dividedown = dividedown + 2.0;	
+
+		}
+		app = app * 4;
+		System.out.println("pi according to Java: "+Math.PI);
+		System.out.println("approximated:     "+ app);
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,27 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String s = args[0];
+            s= s.toUpperCase();
+            int num = Integer.parseInt(args[1]);
+            int l = s.length();
+            for(int i=0; i<l;i++)
+            {
+                char letter = s.charAt(i);
+                if(letter=='A'|letter=='E'|letter=='F'|letter=='H'|letter=='I'|letter=='L'|letter=='M'|letter=='N'|letter=='O'|letter=='R'|letter=='S'|letter=='X'){
+                        System.out.println("Give me an "+ letter+ ": "+letter+"!");
+                }
+                else{
+                        System.out.println("Give me a "+ letter+ ": "+letter+"!");
+
+                }
+
+            }
+            System.out.println("What does that spell?");
+            for(int j =0; j<num; j++){
+                System.out.println(s + "!!!");
+            }
+
+            
         }
 }
